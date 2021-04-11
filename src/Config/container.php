76b599d80f0052a8a21f6b950bdebd6a4a4b164a
Alexandre Cavanna <alexandre.cavanna.pro@gmail.com>
2021-04-11 18:00:09 +0200

    feat(error): add page error 500 and 404

--- src/Config/container.php
+++ src/Config/container.php
@@ -2,10 +2,8 @@
 
 namespace App\Config;
 
-use Exception;
 use FireStorm\Framework;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
@@ -16,76 +14,57 @@ use Symfony\Component\Routing;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-class Container
-{
-    private ContainerBuilder $containerBuilder;
+$containerBuilder = new ContainerBuilder();
 
-    /**
-     * Container constructor.
-     * @param ContainerBuilder $containerBuilder
-     */
-    public function __construct(ContainerBuilder $containerBuilder)
-    {
-        $this->containerBuilder = $containerBuilder;
-    }
+$loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__));
+$loader->load('services.yml');
 
-    /**
-     * @return ContainerBuilder
-     */
-    public function build(): ContainerBuilder
-    {
-        $loader = new YamlFileLoader($this->containerBuilder, new FileLocator(__DIR__));
-        $loader->load('services.yml');
+$containerBuilder->register('context', Routing\RequestContext::class);
 
-        $this->containerBuilder->register('context', Routing\RequestContext::class);
-        $this->containerBuilder->register('matcher', Routing\Matcher\UrlMatcher::class)
-            ->setArguments(['%routes%', new Reference('context')])
-        ;
-        $this->containerBuilder->register('request_stack', HttpFoundation\RequestStack::class);
-        $this->containerBuilder->register('controller_resolver', HttpKernel\Controller\ControllerResolver::class);
-        $this->containerBuilder->register('argument_resolver', HttpKernel\Controller\ArgumentResolver::class);
+$containerBuilder->register('matcher', Routing\Matcher\UrlMatcher::class)
+    ->setArguments(['%routes%', new Reference('context')])
+;
 
-        $this->containerBuilder->register('listener.router', HttpKernel\EventListener\RouterListener::class)
-            ->setArguments([new Reference('matcher'), new Reference('request_stack')])
-        ;
-        $this->containerBuilder->register('listener.response', HttpKernel\EventListener\ResponseListener::class)
-            ->setArguments(['UTF-8'])
-        ;
+$containerBuilder->register('request_stack', HttpFoundation\RequestStack::class);
+$containerBuilder->register('controller_resolver', HttpKernel\Controller\ControllerResolver::class);
+$containerBuilder->register('argument_resolver', HttpKernel\Controller\ArgumentResolver::class);
 
-        $this->containerBuilder->register('dispatcher', EventDispatcher::class)
-            ->addMethodCall('addSubscriber', [new Reference('listener.router')])
-            ->addMethodCall('addSubscriber', [new Reference('listener.response')])
-        ;
-        $this->containerBuilder->register(Framework::class, Framework::class)
-            ->setArguments([
-                new Reference('dispatcher'),
-                new Reference('controller_resolver'),
-                new Reference('request_stack'),
-                new Reference('argument_resolver'),
-            ])
-        ;
+$containerBuilder->register('listener.router', HttpKernel\EventListener\RouterListener::class)
+    ->setArguments([new Reference('matcher'), new Reference('request_stack')])
+;
 
-        $this->containerBuilder->register('loader', FilesystemLoader::class)
-            ->setArguments(['../views/'])
-        ;
+$containerBuilder->register('listener.response', HttpKernel\EventListener\ResponseListener::class)
+    ->setArguments(['UTF-8'])
+;
 
-        $this->containerBuilder->register('twig', Environment::class)
-            ->setArguments([new Reference('loader'), [
-                'cache' => '../cache/twig',
-                'auto_reload' => true
-            ]])
-        ;
+$containerBuilder->register('listener.exception', HttpKernel\EventListener\ErrorListener::class)
+    ->setArguments(['App\Controller\ErrorController::exception'])
+;
 
+$containerBuilder->register('dispatcher', EventDispatcher::class)
+    ->addMethodCall('addSubscriber', [new Reference('listener.router')])
+    ->addMethodCall('addSubscriber', [new Reference('listener.response')])
+    ->addMethodCall('addSubscriber', [new Reference('listener.exception')])
+;
 
-        return $this->getContainerBuilder();
-    }
+$containerBuilder->register('framework', Framework::class)
+    ->setArguments([
+        new Reference('dispatcher'),
+        new Reference('controller_resolver'),
+        new Reference('request_stack'),
+        new Reference('argument_resolver'),
+    ])
+;
 
-    /**
-     * @return ContainerBuilder
-     */
-    private function getContainerBuilder(): ContainerBuilder
-    {
-        return $this->containerBuilder;
-    }
+$containerBuilder->register('loader', FilesystemLoader::class)
+    ->setArguments(['../views/'])
+;
 
-}
+$containerBuilder->register('twig', Environment::class)
+    ->setArguments([new Reference('loader'), [
+        'cache' => '../cache/twig',
+        'auto_reload' => true
+    ]])
+;
+
+return $containerBuilder;
---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\HttpFoundation\Response;
+
+class ErrorController extends AbstractController
+{
+    /**
+     * @param FlattenException $exception
+     * @return Response
+     */
+    public function exception(FlattenException $exception): Response
+    {
+        $msg = $exception->getMessage();
+        $statusCode = $exception->getStatusCode();
+        return new Response($this->render('error/error.html.twig', [
+            'msg' => $msg,
+            'statusCode' => $statusCode
+        ]));
+    }
+}
